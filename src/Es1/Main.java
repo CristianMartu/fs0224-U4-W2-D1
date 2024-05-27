17dcb40b95f330721d2091c19f668456cf0f4fd8
CristianMartu <martuccicristian18@gmail.com>
2024-05-27 16:52:31 +0200

    logger

--- src/Es1/Main.java
+++ src/Es1/Main.java
@@ -1,11 +1,16 @@
 package Es1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         int[] arrayInt = new int[5];
         for (int i = 0; i < arrayInt.length; i++) {
@@ -17,19 +22,20 @@ public class Main {
         int pos = 1;
         do {
             try {
-                System.out.print("\n" + "Inserisci un numero da 1 a 5 \n"
+                logger.info("Inserisci un numero da 1 a 5 \n"
                         + "per scegliere la posizione, o 0 per terminare: ");
                 pos = scanner.nextInt();
-                System.out.print("inserisci nuovo numero: ");
+                logger.info("inserisci nuovo numero: ");
                 int num = scanner.nextInt();
                 if (pos < 0 | pos > 5) {
-                    System.out.println("Index " + num + " out of bounds for length 5");
+                    logger.info("Index " + num + " out of bounds for length 5");
+
                 } else {
                     arrayInt[pos] = num;
-                    System.out.println(Arrays.toString(arrayInt));
+                    logger.info(Arrays.toString(arrayInt));
                 }
             } catch (InputMismatchException error) {
-                System.out.println(error);
+                logger.error(String.valueOf(error));
                 scanner.next();
             }
         } while (pos != 0);
